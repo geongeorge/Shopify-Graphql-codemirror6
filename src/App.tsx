db8a67862190371e90512ce9b4f3d79fca255fbf
Geon George <geongeorge98@gmail.com>
2023-07-08 16:46:31 +0530

    Initializes Shopify GQL cm6

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import GQLEditor from "./components/GQLEditor";
+
+function App() {
+  return (
+    <div className="w-full">
+      <h1>Shopify Admin GQL</h1>
+      <GQLEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GQLEditor.tsx
@@ -0,0 +1,56 @@
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { graphql } from "cm6-graphql";
+import { githubLight } from "@uiw/codemirror-theme-github";
+import { useEffect, useState } from "react";
+import { GraphQLSchema, IntrospectionQuery, buildClientSchema } from "graphql";
+
+// https://discuss.codemirror.net/t/highlighting-that-seems-ignored-in-cm6/4320/21
+// Dont have @codemirror/basic-setup installed. read last comment in above link
+
+const GQLEditor = () => {
+  const [value, setValue] = useState("");
+  const [schema, setSchema] = useState<GraphQLSchema | undefined>();
+
+  useEffect(() => {
+    void fetch("/schema.json")
+      .then((res) => res.text())
+      .then((schema) => {
+        try {
+          const queryRaw = JSON.parse(schema) as IntrospectionQuery;
+          const schemaBuilt = buildClientSchema(queryRaw);
+          console.log("built", schemaBuilt);
+          setSchema(schemaBuilt);
+        } catch (e) {
+          console.error("Cannot build query", e);
+        }
+      });
+  }, []);
+
+  return (
+    <div className="w-full">
+      {schema && (
+        <ReactCodeMirror
+          value={value}
+          height="600px"
+          width="100%"
+          theme={githubLight}
+          onChange={setValue}
+          basicSetup={{
+            foldGutter: false,
+            dropCursor: false,
+            allowMultipleSelections: false,
+            indentOnInput: false,
+            syntaxHighlighting: true,
+            autocompletion: true,
+            bracketMatching: true,
+            closeBrackets: true,
+            history: true,
+          }}
+          extensions={[graphql(schema)]}
+        />
+      )}
+    </div>
+  );
+};
+
+export default GQLEditor;
